3cda3693ac6a239733874c2a4ec702503113b3a1
Megan <megan.hagar@gmail.com>
2022-06-14 21:45:29 -0700

    Added comments

--- lib/dijkstra.py
+++ lib/dijkstra.py
@@ -8,16 +8,22 @@ def dijkstra(adjacency_matrix, start_node):
 
     while traversal_queue:
         current_node = traversal_queue.pop(0) 
-        
+
+        # check if we looked at this node's adjacency list yet, if we have, skip
         if current_node in visited_nodes:
             continue
 
         visited_nodes.add(current_node)
 
+        # iterate through the current nodes adjacency matrix
         for node, cost in enumerate(adjacency_matrix[current_node]): 
+            # if there is a relationship to another node, add it to queue
             if cost != 0:
                 traversal_queue.append(node)
 
+                # calculate the distance from current node to each additional connection
+                # if the distance is less than shortest distance to that node already, or 
+                # there is no recorded distance to that node, update distance list, and parent list
                 current_distance = cost + shortest_distances[current_node] 
                 if shortest_distances[node] == float('inf') or shortest_distances[node] > current_distance:
                     parent_list[node] = current_node 
